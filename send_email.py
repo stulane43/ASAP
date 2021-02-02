fc670bfd0df781892395473a855862350e85fc4e
Stuart Lane <slane@stuarts-mbp.myfiosgateway.com>
2021-02-02 18:42:11 -0500

    fixing smtp error

--- send_email.py
+++ send_email.py
@@ -4,12 +4,15 @@ import config
 port = 587
 server = smtplib.SMTP(config.mailServer, port)
 context = ssl.create_default_context()
-server.ehlo()
-server.starttls(context=context)
-server.ehlo
-server.login(config.username, config.pwd)
+
+def createServer():
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo
+    server.login(config.username, config.pwd)
 
 def send_mailBB():
+    createServer()
     subject = config.BB_prod["name"] + " is Now Available!"
     body = "Your Item is now available at: \n" + config.BB_prod["url"]
     msg = f"subject: {subject}\n\n{body}"
@@ -20,7 +23,8 @@ def send_mailBB():
         msg
     )
     
-def send_mailGS():    
+def send_mailGS():
+    createServer()
     subject = config.GS_prod["name"] + " is Now Available!"
     body = "Your Items is now available at: \n" + config.GS_prod["url"]
     msg = f"subject: {subject}\n\n{body}"
@@ -32,6 +36,7 @@ def send_mailGS():
     )
 
 def send_mailUpdate(product_update):
+    createServer()
     subject =  "ASAP Update"
     body = product_update
     msg = f"subject: {subject}\n\n{body}"
